741d043b9febe48d32cd1bd5170d5c9938544870
vapirovschi <vapirovschi@arezzosky.com>
2021-09-17 16:21:24 +0300

    update

--- webApp/src/App.js
+++ webApp/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   useEffect(() => {
     //paste here after you deploy de cdk infra
-    const url = ""
+    const url = "https://f7go511t91.execute-api.eu-west-1.amazonaws.com/prod/"
     fetch(url)
       .then(response => response.json())
       .then(data => setMessage(JSON.stringify(data)))
